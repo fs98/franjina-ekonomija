1dc6bbb6215bd81cd34c4b2bed23de1b0e9e2f60
fs98 <58662488+fs98@users.noreply.github.com>
2021-03-17 15:43:46 +0100

    -Sorted all files (images/icons); Black overlay added on carousel in About us page; About us page design improved

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -12,19 +12,19 @@
 			<!-- Row -->
 			<div class="row">
 				
-				<div class="col-12 w-100 text-center mb-5">
+				<div class="col-12 w-100 text-center mb-4">
 					
-					<h1 class="mb-5">
+					<h1>
 						<span class="yellow-border-heading pb-1">Economy of Francesko</span>
 					</h1>
 
 				</div>
 
-				<div class="col-md-9 col-12 d-flex align-items-center justify-content-center text-center">
+				<div class="col-md-9 col-12 d-flex align-items-end justify-content-center text-center">
 					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy </p>
 				</div>
 
-				<div class="col-md-3 col-12 d-flex mt-3 mt-mt-0 align-items-center justify-content-md-end justify-content-center">
+				<div class="col-md-3 col-12 d-flex mt-3 mt-mt-0 align-items-center justify-content-md-end justify-content-start">
 					<img src="{{ asset('images/logo/transparent-logo.svg') }}" class="transparent-logo img-fluid">
 				</div>
 
@@ -34,9 +34,9 @@
 			<!-- Row -->
 			<div class="row mt-5">
 				
-			<div class="col-12 w-100 text-left mt-4 mb-2">
+			<div class="col-12 w-100 text-left my-4 mb-2">
 				
-				<h2 class="">
+				<h2 class="mb-4 mt-2">
 					<span class="yellow-border-heading pb-1">Poruka pape Franje</span>
 				</h2>
 
@@ -46,7 +46,7 @@
 				<img src="{{ asset('images/about/800x500.png') }}" class="img-fluid">
 			</div>
 
-			<div class="col-md-12 col-lg-5 d-flex align-items-center">
+			<div class="col-md-12 col-lg-5 mt-lg-0 mt-5 d-flex align-items-center position-relative">
 				<img src="{{ asset('icons/common/quote.svg') }}" class="about-section-image">
 				<p class="ml-4" style="font-size: 16px;">
 					<strong>Lorem ipsum</strong> dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
@@ -57,9 +57,9 @@
 			<!-- /.Row -->
 			
 			<!-- Row -->
-			<div class="row mt-3">
+			<div class="row mt-5">
 
-			<div class="col-md-12 col-lg-5 d-flex align-items-center order-12 order-lg-1">
+			<div class="col-md-12 col-lg-5 mt-lg-0 mt-5 d-flex align-items-center order-12 order-lg-1 position-relative">
 				<img src="{{ asset('icons/common/quote.svg') }}" class="about-section-image">
 				<p class="ml-4" style="font-size: 16px;">
 					<strong>Lorem ipsum</strong> dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
@@ -77,7 +77,7 @@
 			<div class="row">
 				
 				<div class="col-12 w-100 text-left my-4">
-					<h2 class="mb-5">
+					<h2 class="mb-4">
 						<span class="yellow-border-heading pb-1">World Family</span>
 					</h2>
 				</div>
@@ -99,13 +99,13 @@
 			<div class="row">
 					
 				<div class="col-12 w-100 text-left my-4">
-					<h2 class="my-5">
+					<h2 class="mt-4 mb-0">
 						<span class="yellow-border-heading pb-1">12 Sela</span>
 					</h2>
 				</div>		
 
 				<div  class="col-12 col-lg-6 d-flex align-items-center">
-					<p class="mr-0 mr-lg-4 mb-5 mb-lg-0">
+					<p class="mr-0 mr-lg-4 mb-5 mb-lg-0 pr-4">
 						Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum 
 					</p>
 				</div>
@@ -132,9 +132,9 @@
 			<!-- Row -->
 			<div class="row">
 				
-				<div class="col-12 w-100 text-center mb-5">
+				<div class="col-12 w-100 text-center my-5">
 					
-					<h1 class="my-5">
+					<h1>
 						<span class="yellow-border-heading pb-1">HUB Croatia</span>
 					</h1>
 
@@ -146,22 +146,22 @@
 			<!-- Row -->
 			<div class="row">
 				
-				<div class="col-12 w-100 text-left mb-4">
-					<h1 class="mb-lg-2 mb-5 mt-2">
+				<div class="col-12 w-100 text-left my-3">
+					<h2 class="mt-4 mb-0">
 						<span class="yellow-border-heading pb-1">Tko smo mi?</span>
-					</h1>
-				</div>		
+					</h2>
+				</div>	
 
-			<div class="col-12 col-lg-6 d-flex align-items-center">
+			<div class="col-12 col-lg-6 d-flex align-items-center position-relative mt-5 mt-lg-0">
 				<img src="{{ asset('icons/common/quote.svg') }}" class="hub-croatia-section-image">
-				<p class="ml-4 ml-sm-4 ml-md-4 mb-5 mb-lg-0 hub-croatia-section-p">
+				<p class="ml-4 ml-sm-4 ml-md-4 mb-5 mb-lg-0 hub-croatia-section-p pr-4">
 					<strong>Economy of Francesco</strong>  je inicijativa pape Franje da okupi mlade ekonomiste (studente, poduzetnike, kreatore promjena) cijelog svijeta koji svojim učenjem i radom žele pridonijeti razvoju pravednije ekonomije.
 				</p>
 			</div>
 
 				<div class="col-12 col-lg-6 d-flex align-items-center">
 					<!-- 16:9 aspect ratio -->
-					<div class="embed-responsive embed-responsive-1by1 ml-0 ml-lg-4 hub-croatia-section-iframe">
+					<div class="embed-responsive embed-responsive-16by9 ml-0 ml-lg-4 hub-croatia-section-iframe">
 					  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/O7RPlFTvxEY" allowfullscreen></iframe>
 					</div>
 				</div>
@@ -173,40 +173,44 @@
 			<div class="row team">
 				
 				<div class="col-12 w-100 text-left mt-3 mb-4">
-					<h1 class="mb-lg-2 mb-5 mt-5">
+					<h2 class="mb-lg-2 mb-5 mt-5">
 						<span class="yellow-border-heading pb-1">Naš tim</span>
-					</h1>
+					</h2>
 				</div>
 
-				<div class="col-12 col-lg-5 d-flex flex-column justify-content-start align-items-center">
+				<div class="col-12 col-xl-5 d-flex flex-column justify-content-start align-items-center">
 				
-					<div class="mt-5 pb-3 d-flex flex-row justify-content-center align-items-center team-group">
+					<div class="d-flex flex-row justify-content-center align-items-center team-group py-2">
+						<img src="{{ asset('images/about/180x180-circle.png') }}" class="img-fluid team-images">
+						<p class="mx-0 mr-2 px-0 team-text align-self-middle text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh </p>
+					</div>
+
+					<div class="d-flex flex-row justify-content-center align-items-center team-group py-2">
 						<img src="{{ asset('images/about/180x180-circle.png') }}" class="img-fluid team-images">
 						<p class="mx-0 px-0 team-text align-self-middle text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh </p>
 					</div>
 
-					<div class="mt-2 pb-3 d-flex flex-row justify-content-center align-items-center team-group">
+					<div class="d-flex flex-row justify-content-center align-items-center team-group py-2">
 						<img src="{{ asset('images/about/180x180-circle.png') }}" class="img-fluid team-images">
 						<p class="mx-0 px-0 team-text align-self-middle text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh </p>
 					</div>
 
-					<div class="mt-2 pb-3 d-flex flex-row justify-content-center align-items-center team-group">
-						<img src="{{ asset('images/about/180x180-circle.png') }}" class="img-fluid team-images">
-						<p class="mx-0 px-0 team-text align-self-middle text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh </p>
-					</div>
-
-					<div class="mt-2 pb-3 d-flex flex-row justify-content-center align-items-center team-group">
+					<div class="d-flex flex-row justify-content-center align-items-center team-group team-group-no-border py-2">
 						<img src="{{ asset('images/about/180x180-circle.png') }}" class="img-fluid team-images">
 						<p class="mx-0 px-0 team-text align-self-middle text-center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh </p>
 					</div>
 
 				</div>	
 
-				<div class="col-12 col-lg-7 d-flex justify-content-end align-items-end">
-					<img src="{{ asset('images/about/image-2.jpg') }}" class="img-fluid">
+				<div class="col-12 col-xl-7 d-flex justify-content-end align-items-end mt-5 mt-xl-0">
+					<img src="{{ asset('images/about/image-2.jpg') }}" class="img-fluid h-100">
 				</div>	
 
 			</div>
+
+			<form>				
+				@csrf
+			</form>
 			<!-- /.Row -->
 
 			<!-- Row -->
@@ -239,9 +243,9 @@
 		    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
 		  </ol>
 		  <div class="carousel-inner">
-		    <div class="carousel-item active position-relative">
+		    <div class="carousel-item active">
 		      <img src="{{ asset('images/about/image-3.jpg') }}" class="d-block w-100" alt="..." >
-		      <div class="black-overlay" style="position: absolute; top: 0; left: 0; height: 100%; width: 100%; background-color: rgba(0,0,0,0.44);"></div>
+		      <div class="black-overlay"></div>
 		      <div class="carousel-caption d-none d-md-block">
 		        <h2 class="mb-5">Naziv sela</h2>
 		        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
